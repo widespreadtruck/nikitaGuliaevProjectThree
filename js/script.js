cd585e42c96334f5272541a48540f8443ac36645
Nikita Guliaev <gulnikita63@gmail.com>
2020-03-17 23:57:38 -0400

    ClientList container gets highlighted properly

--- js/script.js
+++ js/script.js
@@ -12,38 +12,48 @@ $(function() {
     //highlight selected buttons and the client list box
     const timeSlotButtons = $(".timeSlotButtons");
 
-    // const dayOfTheClass = function() {
-    //     for (let item in weekClientList){
-    //         console.log(item);
-    //         if (item == ){
-                    //ConditionRule;
-    //         }
-    //     }
-    // };
-
-    // $(".dayContainerHeaderBoxParagraphs").text();
-
     timeSlotButtons.on("click", function(){
         //highlights a time slot button when clicked
         $(this).toggleClass("selectedButtonChangesColor");
         //highlight the client list container when a time slot button is selected
-        $(".clientListContainer").toggleClass("thisClassWillBeToggledToHighlightClientListBox");
+
         //When a time slot is selected, travel up the DOM until find the closest class="dayContainer", then traverse down until find the closest class="dayContainerHeaderBoxParagraphs" and copy the text - Day of the week.
+        
         const grabsTheClosestDayValue = $(this).closest(".dayContainer").find(".dayContainerHeaderBoxParagraphs").text();
         //copy the time period from the button
         const timeOfClass = $(this).text();
+
+        console.log("");
+
+        if ($(this).css("background-color") == "rgb(209, 119, 5)"){
+            $(".clientListContainer").css("background-color", "rgba(209, 119, 5, 0.2)");
+            //insert the Day and Time values in the Header of the client list container
+            $(".dayOfTheClassOnClientList").text(grabsTheClosestDayValue).css("color", "rebeccapurple");
+            $(".classTimeSlot").text(timeOfClass).css("color", "rebeccapurple");
+        } else {
+            $(".clientListContainer").css("background-color", "transparent");
+            $(".dayOfTheClassOnClientList").empty();
+            $(".classTimeSlot").empty();                       
+        }
+
+        // $(this).toggle(function(){
+        // }, function(){
+        //     //remove highlighting of client list container
+        //     $(".clientListContainer").toggleClass("thisClassWillBeToggledToHighlightClientListBox");
+            
+        //     $(".dayOfTheClassOnClientList").text();
+            
+        //     $(".classTimeSlot").text();
+
+        // })
+
+
+
         console.log(grabsTheClosestDayValue);
         console.log(timeOfClass);
-        //insert the Day and Time values in the Header of the client list container
-        $(".dayOfTheClassOnClientList").text(grabsTheClosestDayValue).css("color", "rebeccapurple");
-        $(".classTimeSlot").text(timeOfClass).css("color", "rebeccapurple");
-
-        
-
 
         //on second click removes the highlighting of the selected TimeSlot button & the client list container
         timeSlotButtons.not(this).removeClass("selectedButtonChangesColor");
-        // .removeClass("thisClassWillBeDisplayedInTheHeaderOfClientListBox");
     });
 
 
@@ -56,6 +66,7 @@ $(function() {
         return prompt("Type in client's name");
     }
 
+    //when "ADD CLIENT" button is clicked 
     $(".addClientButton").on("click", function(event){
         //preventing default
         event.preventDefault();
@@ -64,22 +75,16 @@ $(function() {
         const grabbedClientName = askForClientName();
 
         //add grabbed name to an array
-        weekClientList.monday.nineToTenAmClass.push(grabbedClientName);
+        weekClientList.Monday.nineToTenAmClass.push(grabbedClientName);
         
         //print the last name added to an array inside of the ClientList box
-        $(".namesAddedToTimeSlotSoFar").append(`<li>${weekClientList.monday.nineToTenAmClass[weekClientList.monday.nineToTenAmClass.length - 1]}</li>`);
+        $(".namesAddedToTimeSlotSoFar").append(`<li>${weekClientList.Monday.nineToTenAmClass[weekClientList.Monday.nineToTenAmClass.length - 1]}</li>`);
 
         //6 ppl is the max, so when the array's length reaches 6, the TimeSlot turns red.
-        if (weekClientList.monday.nineToTenAmClass.length === 6) {
+        if (weekClientList.Monday.nineToTenAmClass.length === 6) {
             alert("Class is full. Congratulations!");
         }
 
-
-        
-
-        
-
-        
 
     })
 
