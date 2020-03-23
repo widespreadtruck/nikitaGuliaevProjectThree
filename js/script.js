0f50b1724c204f64306b3be338846923e92ed072
Nikita Guliaev <gulnikita63@gmail.com>
2020-03-23 14:30:56 -0400

    troubleshooting

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ $(function() {
     //array with client names
     const weekClientList = {
         Monday: {
-            nineToTenAmClass: ["fuck", "ass"],
-            sixToSevenPmClass: ["lol", "shit"]
+            nineToTenAmClass: ["test1.1", "test1.2"],
+            sixToSevenPmClass: ["test2.1", "test2.2"]
         },
         Tuesday: {
             nineToTenAmClass: [],
@@ -44,24 +44,28 @@ $(function() {
         const toggledObject = $(".selectedButtonChangesColor");
         console.log(`length of toggled class: ${toggledObject.length}`);
 
-
+        //if toggled class exists in the DOM do the following:
         if (toggledObject.length === 1) {
+            //find and save a text node representing the DAY:
             const dayForTheArrayNavigation = $(".selectedButtonChangesColor").closest(".dayContainer").find(".dayContainerHeaderBoxParagraphs").text();
             console.log(`dayForTheArrayNavigation: ${dayForTheArrayNavigation}`); //Monday
-    
+
+            //find and save a text node representing the TIME:
+            const timeForTheArrayNavigation = $(".selectedButtonChangesColor").closest(".additonalClassForClosestMethod").find(".hiddenClassForArray").text();
+            console.log(`timeForTheArrayNavigation: ${timeForTheArrayNavigation}`); //nineToTenAmClass
+
+            //find and save a text node representing the text written on the selected button:
             const timeOnTheTimeSlotButton = $(".selectedButtonChangesColor").closest(".timeSlotButtons").text();
             console.log(`timeOnTheTimeSlotButton: ${timeOnTheTimeSlotButton}`); //9am-10am | 0/6
     
-            const timeForTheArrayNavigation = $(".selectedButtonChangesColor").closest(".additonalClassForClosestMethod").find(".hiddenClassForArray").text();
-            console.log(`timeForTheArrayNavigation: ${timeForTheArrayNavigation}`); //nineToTenAmClass
     
             const arrayPathThreeLevelsDeep = weekClientList[dayForTheArrayNavigation][timeForTheArrayNavigation];
             console.log(arrayPathThreeLevelsDeep);
 
             $(".clientListContainer").css("background-color", "rgba(209, 119, 5, 0.2)");
-            //insert the Day and Time values in the Header of the client list container
-            // console.log(`timeSlotButtonsBackgroundColor: ${timeSlotButtonsBackgroundColor}`);
-            //rgb(209, 119, 5)
+            // //insert the Day and Time values in the Header of the client list container
+            // // console.log(`timeSlotButtonsBackgroundColor: ${timeSlotButtonsBackgroundColor}`);
+            // //rgb(209, 119, 5)
 
             selectSpanForDay.text(dayForTheArrayNavigation).css("color", "blue");
             selectSpanForTime.text(timeOnTheTimeSlotButton).css("color", "blue");
@@ -75,32 +79,35 @@ $(function() {
                 placeForTheListOfClients.append(`<li>${arrayPathThreeLevelsDeep[i]}</li>`);
             };
 
+
             addClientButton.on("click", function (event) {
                 //preventing default
-                // event.preventDefault();
+                event.preventDefault();
                 //DO I NEED a "PREVENTDEFAULT" HERE?
 
+                console.log(event.target);
                 const askForClientName = function () {
-                    return prompt("Type in client's name");
+                    prompt("Type in client's name");
                 };
 
-                //grab name from prompt
+                //grabs name from prompt
                 const grabbedClientName = askForClientName();
 
-                arrayPathThreeLevelsDeep.push(grabbedClientName);
-                console.log(grabbedClientName);
-                console.log(arrayPathThreeLevelsDeep);
+                // //adds the grabbed name to the array
+                // arrayPathThreeLevelsDeep.push(grabbedClientName);
+                // console.log(grabbedClientName);
+                // console.log(arrayPathThreeLevelsDeep);
 
-                // placeForTheListOfClients.empty();
+                // // placeForTheListOfClients.empty();
 
-                const arrayLength = arrayPathThreeLevelsDeep.length - 1;
-                console.log(`arrayLength: ${arrayLength}`);
-                placeForTheListOfClients.append(`<li>${arrayPathThreeLevelsDeep[arrayLength]}</li>`);
+                // const arrayLength = arrayPathThreeLevelsDeep.length - 1;
+                // console.log(`arrayLength: ${arrayLength}`);
+                // placeForTheListOfClients.append(`<li>${arrayPathThreeLevelsDeep[arrayLength]}</li>`);
 
-                if (arrayPathThreeLevelsDeep.length === 6) {
-                    alert("Class is full. Congratulations!");
-                };
-                console.log(arrayPathThreeLevelsDeep.length);
+                // if (arrayPathThreeLevelsDeep.length === 6) {
+                //     alert("Class is full. Congratulations!");
+                // };
+                // console.log(arrayPathThreeLevelsDeep.length);
             });
 
         } else {
@@ -113,3 +120,6 @@ $(function() {
         };
     });
 });
+
+
+//why am I getting prompt three times in a row?
